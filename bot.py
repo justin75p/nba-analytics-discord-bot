19217830dade39c2433fcba427a61744414d3943
Justin <justin75p@gmail.com>
2025-09-01 14:11:46 -0700

    add on ready event handler and a basic echo chat command

--- bot.py
+++ bot.py
@@ -4,13 +4,24 @@ import logging
 from dotenv import load_dotenv
 import os
 
+# Load token
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
+# Set up bot
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
-intents.members = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} has connected!")
+
+@bot.command()
+async def echo(ctx, *, arg):
+    await ctx.send(arg)
+
+
+bot.run(token = token, log_handler = handler, log_level = logging.DEBUG)
