494f407c7649e857741baea406f49836da00fab3
Justin <justin75p@gmail.com>
2025-09-01 16:18:13 -0700

    add basic active player lookup command for bot

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import logging
 from dotenv import load_dotenv
 import os
 
+from nba_api.stats.endpoints import playergamelog
+from nba_api.stats.static import players
+
 # Load token
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
@@ -22,6 +25,25 @@ async def on_ready():
 @bot.command()
 async def echo(ctx, *, arg):
     await ctx.send(arg)
+
+@bot.command()
+async def player(ctx, *, player_name):
+    # Find a player by their full name
+    player_list = players.find_players_by_full_name(player_name)
+    if not player_list:
+        await ctx.send(f"Player {player_name} not found.")
+        return
+
+    active_players = [player for player in player_list if player['is_active']]
+    if not active_players:
+        await ctx.send(f"No active player named {player_name} found.")
+        return
+    
+    # As of 2025, no active players share the exact same names
+    player = active_players[0]
+    player_id = player['id']
+
+    await ctx.send(f"Found active player {player['full_name']} with id {player_id}")
 
 
 bot.run(token = token, log_handler = handler, log_level = logging.DEBUG)
